4cba238e7b214e4d7bf64040dfb5aff3247e805d
Fatima <76473050+fabacha@users.noreply.github.com>
2023-06-09 10:55:14 +0100

    Update base_model.py

--- base/base_model.py
+++ base/base_model.py
@@ -1,25 +1,32 @@
+import torch
 import torch.nn as nn
-import numpy as np
-from abc import abstractmethod
+import torch.nn.functional as F
 
 
-class BaseModel(nn.Module):
-    """
-    Base class for all models
-    """
-    @abstractmethod
-    def forward(self, *inputs):
-        """
-        Forward pass logic
+class CNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
 
-        :return: Model output
-        """
-        raise NotImplementedError
+        self.classifier = nn.Linear(32 * 8 * 8, num_classes)
 
-    def __str__(self):
-        """
-        Model prints with number of trainable parameters
-        """
-        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
-        params = sum([np.prod(p.size()) for p in model_parameters])
-        return super().__str__() + '\nTrainable parameters: {}'.format(params)
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+if __name__ == "__main__":
+    net = CNN(num_classes=10)
+    net.eval()
+    torch.manual_seed(1)
+    x = torch.randn(10, 3, 32, 32)
+    out = net(x)
+    print("out.shape:", out.shape)
