b44de5a71099519a8e0f373303ad75ad8a2780c7
Fatima <76473050+fabacha@users.noreply.github.com>
2023-06-19 17:43:51 +0100

    Update cnn_flame.py

--- models_flame/cnn_flame.py
+++ models_flame/cnn_flame.py
@@ -2,50 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# class CNN(nn.Module):
-#     def __init__(self, num_classes=10):
-#         super(CNN, self).__init__()
-#         self.features = nn.Sequential(
-#             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2),
-#             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2),
-#         )
-#
-#         self.classifier = nn.Linear(32 * 8 * 8, num_classes)
-#
-#     def forward(self, x):
-#         x = self.features(x)
-#         x = x.view(x.size(0), -1)
-#         x = self.classifier(x)
-#         return x
-#
-
-#Better Accuracy
-
-# class CNN(nn.Module):
-#     def __init__(self, num_classes=10):
-#         super(CNN, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-#         self.relu = nn.ReLU()
-#         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-#         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-#         self.fc = nn.Linear(32 * 8 * 8, num_classes)
-#
-#     def forward(self, x):
-#         out = self.conv1(x)
-#         out = self.relu(out)
-#         out = self.maxpool(out)
-#         out = self.conv2(out)
-#         out = self.relu(out)
-#         out = self.maxpool(out)
-#         out = out.view(out.size(0), -1)
-#         out = self.fc(out)
-#         return out
-
-
 class BasicBlock(nn.Module):
     expansion = 1
 
